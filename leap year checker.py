30e7d1c1337cbeffdb8d2181133912bc8afde847
niyaziozkan <75857630+niyaziozkan@users.noreply.github.com>
2020-12-20 16:44:52 +0300

    Update leap year checker.py

--- leap year checker.py
+++ leap year checker.py
@@ -8,6 +8,6 @@ year = int(input("Please enter a 4-digit year: "))
 divide_4 = year % 4
 divide_100 = year % 100
 divide_400 = year % 400
-leap_year = (not divide_4 and not divide_100 and not divide_400) or             (bool(divide_100) and bool(divide_400) and bool(not divide_4))
+leap_year = (not divide_4 and not divide_100 and not divide_400) or (bool(divide_100) and bool(divide_400) and bool(not divide_4))
 print(leap_year)
 
